5168e785ce85876e78647240d6420848cc146030
Kevin <herrbach.kevin@gmail.com>
2021-11-24 15:45:54 +0900

    Adds gh page and default toolbar

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,25 @@
   <div id="app">
     <v-app>
       <v-main>
+        <v-toolbar class="px-5">
+          <v-row align="center">
+            <v-col cols="3">
+              <v-btn icon x-large><v-icon>mdi-instagram</v-icon></v-btn>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <v-toolbar-title>L'aventure Coréene par Kebinou</v-toolbar-title>
+            </v-col>
+            <v-col cols="3">
+              <v-row justify="end">
+                <v-btn text plain x-small>Categories</v-btn>
+                <v-btn text plain x-small>Tous les articles</v-btn>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-toolbar>
         <router-view/>
       </v-main>
     </v-app>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <v-container>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row>
+      <v-col></v-col>
+      <v-col></v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
