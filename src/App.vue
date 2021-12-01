92c3b8cc62e22b5442e6b6a9dd466e29a2b2c56f
Kevin <herrbach.kevin@gmail.com>
2021-12-01 17:44:29 +0900

    Changes colors in app bar

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,23 @@
   <div id="app">
     <v-app>
       <v-main>
-        <v-app-bar class="px-5" flat fixed color="white">
+        <v-app-bar class="px-5"  fixed color="white" elevate-on-scroll>
           <v-row align="center">
             <v-col cols="3">
-              <v-btn icon x-large href="https://www.instagram.com/kebinou_korea/" target="_blank">
-                <v-icon>mdi-instagram</v-icon>
+              <v-btn icon x-large color="error"
+                     href="https://www.instagram.com/kebinou_korea/" target="_blank">
+                <v-icon color="grey">mdi-instagram</v-icon>
               </v-btn>
             </v-col>
             <v-col cols="6" class="text-center">
-              <h3 class="main-title font-weight-medium">L'aventure Coréene par Kebinou</h3>
+              <h1 class="main-title font-weight-medium">L'aventure Coréenne de Kebinou</h1>
             </v-col>
             <v-col cols="3">
               <v-row justify="end">
-                <v-btn text plain x-small @click="$vuetify.goTo('#categories')">Categories</v-btn>
-                <v-btn text plain x-small @click="$vuetify.goTo('#articles')">Tous les articles</v-btn>
+                <v-btn text plain x-small
+                       @click="$vuetify.goTo('#categories')">Categories</v-btn>
+                <v-btn text plain x-small
+                       @click="$vuetify.goTo('#articles')">Tous les articles</v-btn>
               </v-row>
             </v-col>
           </v-row>
@@ -30,5 +33,6 @@
 @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
 .main-title {
   font-family: 'Lobster', cursive;
+  color: #1976D2;
 }
 </style>
