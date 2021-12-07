df4ec41484672c4293cda21d439ca52402236aeb
Kevin <herrbach.kevin@gmail.com>
2021-12-07 10:16:05 +0900

    Updates article

--- src/components/Articles/HotteokArticle.vue
+++ src/components/Articles/HotteokArticle.vue
@@ -22,40 +22,44 @@
                     <v-img src="@/assets/articles/hotteok/stand.jpg" height="400" contain/>
                     <p>L'origine semble venir de travailleurs Chinois ayant debarqués en Corée du Sud en 1927
                     et ayant rapidement développés pleins de recette de plats peu chers afin de pouvoir les
-                    vendre aux plus pauvres. L'un d'eux était le 호떡. <i>Je parle ici de l'origine du 호떡 qu'on
+                    vendre aux plus pauvres. L'un d'eux était le 호떡.<br><i>Je parle ici de l'origine du 호떡 qu'on
                     trouve partout en Corée aujourd'hui. Il semblerait que le vrai 호떡 voit son origine remonter
                     à la fin du 19ème siècle.</i></p>
                     <p>La préparation est assez simple: Farine de blé, lait, sucre, levure pour la pâte, qui peut
                         ensuite être farçie avec un mélange de plusieurs aliments dont :
-                        cassonade, miel, cacahuètes, cannelle et graines de sésame. Le tout est ensuite frit.</p>
+                        cassonade, miel, cacahuètes, cannelle et graines de sésame.<br>Le tout est ensuite frit.</p>
                     <v-img src="@/assets/articles/hotteok/hotteok.jpg" height="400" contain/>
                     <p>Quand j'ai entendu parler du 호떡 la première fois, j'ai évidemment voulu me jetter sur
-                        le premier que je croiserais, étant un graaaaaaand amateur de crêpes.
-                        C'est donc autour de mon quartier d'Hongdae que je crois le Graal un soir. Et j'avoue
-                        avoir été un peu déçu. Très peu de garniture, trop cuit, les yeux doux de la mamie tenant
-                        le stand ne m'auront pas reconfortés de la petite déception que je venais de vivre je l'avoue.
+                      le premier que je croiserais, étant un <span class="font-weight-bold">graaaaaaand</span> amateur de crêpes.
+                        C'est donc autour de mon quartier d'Hongdae que je rencontre le Graal un soir. Et j'avoue
+                        avoir été un peu déçu :<br> Très peu de garniture et trop cuit, plus proche du carton que d'un dessert.
+                        Les yeux doux de la mamie tenant le stand ne m'auront pas reconfortés de la petite déception
+                        que je venais de vivre je l'avoue.<br>
                         Maaaaaais, je ne m'avoue pas vaincu et le second que je tente ailleurs était succulent.
-                        Le prix est en général de 1000₩, mais est sujet à variations notamment si vous tenez un
+                        Le prix est en général de 1000₩ (~0.75€), mais est sujet à variations notamment si vous tenez un
                         호떡 avec un goût différent de l'original.</p>
                     <p>En parlant de ça (quelle transition), j'avais justement entendu parler d'une légende,
-                    d'un mythe, un 호떡 rare qui donnerait un ticket allé-simple pour le paradis. J'ai nommé :
-                    le 호똑 au Nutella. Celui-ci se trouvant dans une petite boutique et non un stand.</p>
+                    d'un mythe, un 호떡 rare qui donnerait un ticket allé-simple pour le paradis en classe business. J'ai nommé :
+                    <span class="font-weight-bold">le 호똑 au Nutella.</span></p>
                     <p>Ni une, ni deux, j'enfourche mon vélo (première fois que je re faisait du vélo en ~15ans)
                     et je pars pour 1h30 de balade au bord de la rivière Han jusqu'à ce sanctuaire du bonheur
                     (il fallait que je dépense un max de kcal avant la rencontre avec ce 호똑, avec qui je déciderai
                     de me marier, ndlr). Et bien la promesse à été tenue, c'est du plaisir pur et dur. Chargé juste
                     ce qu'il faut en Nutella. Je n'ai pas envie de trop spoil celles et ceux qui testeront un jours,
                     mais on imagine très bien à quel point j'ai longuement hesité à m'installer définitevement dans
-                    cette boutique.</p>
+                    cette boutique.<br><i>Oui celui-là est un peu célèbre et n'est plus dans un stand mais une boutique carrément.</i></p>
                     <v-img src="@/assets/articles/hotteok/video.gif" height="400" contain/>
-                    <p>Envoyez-moi un message, taggez-moi sur vos story insta si vous allez tester un 호떡 hehe! Bisous.</p>
+                    <p>Envoyez-moi un message, taggez-moi sur vos story insta
+                      (<a href="https://www.instagram.com/kebinou_korea/" target="_blank">@kebinou_korea</a>)
+                      si vous allez tester un 호떡 hehe!<br> Bisous.</p>
                     <v-card>
                         <v-card-title>Infos :</v-card-title>
+                        <v-divider></v-divider>
                         <v-card-text class="d-flex flex-column">
-                            <div class="text-h6">Nom : 삼맛호오떡</div>
-                            <div class="text-h6">Prix : 3000₩</div>
-                            <div class="text-h6">Téléphone : 02-2299-3396</div>
-                            <div class="text-h6">Naver map: <a href="http://naver.me/GgeusipV" target="_blank">Map</a></div>
+                          <div class="text-h6">Nom : <span class="article__primary-text font-weight-bold">삼맛호오떡</span></div>
+                          <div class="text-h6">Prix : <span class="article__primary-text font-weight-bold">3000₩ (~2€)</span></div>
+                          <div class="text-h6">Téléphone : <span class="article__primary-text font-weight-bold">02-2299-3396</span></div>
+                          <div class="text-h6">Naver map: <a href="http://naver.me/GgeusipV" target="_blank">Map</a></div>
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -81,10 +85,13 @@
             font-size: 1.5em;
             font-weight: lighter;
             letter-spacing: 0.1em;
-            line-height: 2em;
+            line-height: 2.5em;
         }
         p {
-            margin: 25px 0;
+            margin: 100px 0;
         }
+      &__primary-text {
+        color: #1976D2;
+      }
     }
 </style>
