f6c7f5a1dbee47ef00c018c30cfe23838381b72d
Kevin <herrbach.kevin@gmail.com>
2022-01-03 12:28:50 +0900

    Fixes a few french errors

--- src/translations/mbc.js
+++ src/translations/mbc.js
@@ -12,24 +12,23 @@ const mbcMessages = {
           <p>Le problème étant que l'année durant laquelle je suis arrivée en Corée, le monde était bouleversé par
             la crise du coronavirus.<br/> Et, pour une bribe de contexte, la Corée était l'un des pays qui gérait le mieux
             l'épidémie à ce moment, alors que la France était plutôt dans la tranche des pays en difficulté.<br/>
-            La République Asiatique avait même complètement restreint l'accès à son territoire aux Français à un moment
+            La République Asiatique a donc complètement restreint l'accès à son territoire aux Français à un moment
             donné, puis avait autorisé une poignée de personne seulement et c'est à ce moment que j'ai pu m'y rendre
-            (oui j'ai eu le cul bordé de nouille 🍜 comme dirait mon père).
-          </p>
+            (oui j'ai eu le cul bordé de nouille 🍜 comme dirait mon père).</p>
           <p>Ceci étant dit, impossible pour MBC de faire venir 2-3 personnes de l'étranger pour 1-2 jours de tournage
             (d'autant plus qu'une quatorzaine était obligatoire).<br/> La production a donc changé
             légèrement le principe de son émission pour suivre uniquement le seul étranger présent.</p>`,
         mbc3: `<p>Je suis donc fraichement arrivé en Corée, ca fait à-peu-près 1 mois que je suis à Séoul et que j'ai commencé à
             bosser.<br/> Un jour je reçois un message assez long, en full-Coréen sur mon insta (alors doté du timide nombre d'environ
-            80 followers (&lt;3 je vous aime)), que j'ignore sans vergogne pensant à un spam me promettant de devenir millionaire ou
-            de découvrir les filles chaudes de ma région. Quelques jours se passent, et boom, rebelote, même message.<br/> Cette
+            80 followers (&lt;3 je vous aime)) que j'ignore sans vergogne pensant à un spam me promettant de devenir millionaire en
+            gagnant près de 10000€ par jour depuis mon canapé. Quelques jours se passent, et boom, rebelote, même message. Cette
             fois-çi, je traduis le message sur Papago (le sang de la veine), après tout, devenir millionaire, pourquoi pas !</p>
           <p>L'auteur du message était selon lui, un employé de MBC, il me décrivait en gros l'émission, me disait qu'ils
-            avaient repéré mon profil et qu'il serait intéressé d'avoir un entretien avec moi.<br/> Je bosse dans l'informatique,
+            avaient repéré mon profil et qu'il serait intéressé d'avoir un entretien avec moi.<br/> Mais je bosse dans l'informatique,
             ou bien est-ce seulement ma nature, mais à ce moment je pense toujours que ce message est un spam et que l'auteur
             va me demander mon numéro de carte bancaire dans le prochain message.<br/> Cependant un léger doute commence à s'installer,
             et je montre donc le message à mes collègues Coréennes. Elles me disent qu'en fait non, le message à l'air d'être
-            bel et bien honnête ! Elles ont l'air même un peu choqué, je l'ignorais mais elles m'apprennent que cette
+            bel et bien honnête ! Elles ont l'air même un peu choqué, je l'ignorais, mais elles m'apprennent que cette
             émission est quand même assez populaire et que tout le monde la connait au moins de nom. Elles n'en reviennent pas,
             et enfait moi non plus.</p>`,
         mbc4: `<p>"L'opportunité est prompte à s'enfuir, mais longue à revenir", cette très ancienne citation de Hazrat Ali résonne
@@ -43,15 +42,15 @@ const mbcMessages = {
             atterris ici.<br/> Par contre le tout était filmé (et pas sur mon bon profil 😭 !), j'ai demandé moi aussi
             si je pouvais me filmer avec ma propre caméra, histoire de vous faire partager ça, mais il m'ont dit non :/.
             L'entretien se passe nickel et ils sont obligé de me mettre dehors à un moment donné parce que
-            je raconte trop ma vie; qu'il est presque 21h et qu'ils doivent encore bosser derrière (les pauvres, force à eux).<br/>
+            je raconte trop ma vie, qu'il est presque 21h et qu'ils doivent encore bosser derrière (les pauvres, force à eux).<br/>
             Pas de conclusion à ce moment là, ils devraient me recontacter dans les jours à venir.</p>
           <p>J'étais déjà hyper content d'être arrivé jusqu'ici et je n'attendais pas de réponse pour être honnête,
-            je n'ai pas le visage hyper symétrique, sur le spectre introverti/extraverti je suis plutôt côté introverti :<br/>
+            je n'ai pas le visage hyper symétrique, sur le spectre introverti/extraverti je suis plutôt côté introverti, en gros :<br/>
             <span class="font-weight-bold">Je ne pense pas être un bon "match" pour faire de la télé.</span></p>`,
-        mbc5: `<p>Et pourtant ! Après quelques jours / semaines, mon contact chez MBC me \\confirme que tout est bon de leur
-            côté et que je vais pouvoir tourner l'épisode dans les jours à venir !<br/> Quelle chance, je n'en reviens pas.
+        mbc5: `<p>Et pourtant ! Après quelques jours / semaines, mon contact chez MBC me confirme que tout est bon de leur
+            côté et que je vais pouvoir tourner l'épisode dans les jours à venir !<br/> Quelle chance ! Ce qui m'arrive est complètement dingue.
             Je suis bien super hyper excité, mais il faut l'avouer également un peu anxieux. Passer devant des centaines
-            de milliers (voir millions) de personne quand on a pas une confiance en soi démesurée, ce n'est pas le chose
+            de milliers (voir millions) de personnes quand on a pas une confiance en soi démesurée, ce n'est pas le chose
             la plus évidente au monde haha.</p>
           <span>Je ne vais pas écrire les détails du tournage dans ce billet, il est déjà trop long et indigeste, mais promis,
             si ça vous intéresse, je l'écrirais du mieux que je peux afin de vraiment vous faire vivre les coulisses de
@@ -61,12 +60,12 @@ const mbcMessages = {
             - Chercher les termes 'Welcome first time' ou directement '어서와 한국은 처음이지'</span>`,
         mbc5_2: `<span>si vous avez un clavier Coréen (ou le copier depuis ce billet 🤭)<br/>
             - Episode 40 de la saison 2021, date exacte : 2021-10-07<br/>
-            - La première partie est sur un Américain, je suis présenet dans l'intro et en deuxième partie d'épisode<br/>
+            - La première partie est sur un Américain, je suis présent dans l'intro et en deuxième partie d'épisode<br/>
             - Préparer vous à "cringe" sévèrement, perso je n'arrive pas à regarder l'épisode tellement je
             suis ridiculus par moment 😂 (mais bon askip' ça ne tue pas).</span>`,
         mbc6: `<p>En bonus: Quelques images de l'épisode et du tournage. Je n'avais pas vraiment le droit de prendre des photos
-            durant le shooting, mais j'ai insister pour quand même prendre quelques-unes.</p>`,
-        mbc7: `<p><i>Au moment où j'écris ce billet, j'ai tourné un autre épisode dispo le 6 Janvier 2022 sur Netflix.</i></p>
+            durant le shooting, mais j'ai insisté pour quand même prendre quelques-unes.</p>`,
+        mbc7: `<p><i>Au moment où j'écris ce billet, j'ai tourné un autre épisode dispo le 6 Janvier 2022 sur Netflix !</i></p>
           <p>Faite moi vos retours les plus honnête sur mon insta si vous avez vu l'épisode :
             (<a href="https://www.instagram.com/kebinou_korea/" target="_blank">@kebinou_korea</a>).
             Ca me fera énormément plaisir ❤. Bisous.</p>`,
@@ -89,24 +88,23 @@ const mbcMessages = {
           <p>Le problème étant que l'année durant laquelle je suis arrivée en Corée, le monde était bouleversé par
             la crise du coronavirus.<br/> Et, pour une bribe de contexte, la Corée était l'un des pays qui gérait le mieux
             l'épidémie à ce moment, alors que la France était plutôt dans la tranche des pays en difficulté.<br/>
-            La République Asiatique avait même complètement restreint l'accès à son territoire aux Français à un moment
+            La République Asiatique a donc complètement restreint l'accès à son territoire aux Français à un moment
             donné, puis avait autorisé une poignée de personne seulement et c'est à ce moment que j'ai pu m'y rendre
-            (oui j'ai eu le cul bordé de nouille 🍜 comme dirait mon père).
-          </p>
+            (oui j'ai eu le cul bordé de nouille 🍜 comme dirait mon père).</p>
           <p>Ceci étant dit, impossible pour MBC de faire venir 2-3 personnes de l'étranger pour 1-2 jours de tournage
             (d'autant plus qu'une quatorzaine était obligatoire).<br/> La production a donc changé
             légèrement le principe de son émission pour suivre uniquement le seul étranger présent.</p>`,
         mbc3: `<p>Je suis donc fraichement arrivé en Corée, ca fait à-peu-près 1 mois que je suis à Séoul et que j'ai commencé à
             bosser.<br/> Un jour je reçois un message assez long, en full-Coréen sur mon insta (alors doté du timide nombre d'environ
-            80 followers (&lt;3 je vous aime)), que j'ignore sans vergogne pensant à un spam me promettant de devenir millionaire ou
-            de découvrir les filles chaudes de ma région. Quelques jours se passent, et boom, rebelote, même message.<br/> Cette
+            80 followers (&lt;3 je vous aime)), que j'ignore sans vergogne pensant à un spam me promettant de devenir millionaire en
+            gagnant près de 10000€ par jour depuis mon canapé. Quelques jours se passent, et boom, rebelote, même message. Cette
             fois-çi, je traduis le message sur Papago (le sang de la veine), après tout, devenir millionaire, pourquoi pas !</p>
           <p>L'auteur du message était selon lui, un employé de MBC, il me décrivait en gros l'émission, me disait qu'ils
-            avaient repéré mon profil et qu'il serait intéressé d'avoir un entretien avec moi.<br/> Je bosse dans l'informatique,
+            avaient repéré mon profil et qu'il serait intéressé d'avoir un entretien avec moi.<br/> Mais je bosse dans l'informatique,
             ou bien est-ce seulement ma nature, mais à ce moment je pense toujours que ce message est un spam et que l'auteur
             va me demander mon numéro de carte bancaire dans le prochain message.<br/> Cependant un léger doute commence à s'installer,
             et je montre donc le message à mes collègues Coréennes. Elles me disent qu'en fait non, le message à l'air d'être
-            bel et bien honnête ! Elles ont l'air même un peu choqué, je l'ignorais mais elles m'apprennent que cette
+            bel et bien honnête ! Elles ont l'air même un peu choqué, je l'ignorais, mais elles m'apprennent que cette
             émission est quand même assez populaire et que tout le monde la connait au moins de nom. Elles n'en reviennent pas,
             et enfait moi non plus.</p>`,
         mbc4: `<p>"L'opportunité est prompte à s'enfuir, mais longue à revenir", cette très ancienne citation de Hazrat Ali résonne
@@ -120,15 +118,15 @@ const mbcMessages = {
             atterris ici.<br/> Par contre le tout était filmé (et pas sur mon bon profil 😭 !), j'ai demandé moi aussi
             si je pouvais me filmer avec ma propre caméra, histoire de vous faire partager ça, mais il m'ont dit non :/.
             L'entretien se passe nickel et ils sont obligé de me mettre dehors à un moment donné parce que
-            je raconte trop ma vie; qu'il est presque 21h et qu'ils doivent encore bosser derrière (les pauvres, force à eux).<br/>
+            je raconte trop ma vie, qu'il est presque 21h et qu'ils doivent encore bosser derrière (les pauvres, force à eux).<br/>
             Pas de conclusion à ce moment là, ils devraient me recontacter dans les jours à venir.</p>
           <p>J'étais déjà hyper content d'être arrivé jusqu'ici et je n'attendais pas de réponse pour être honnête,
-            je n'ai pas le visage hyper symétrique, sur le spectre introverti/extraverti je suis plutôt côté introverti :<br/>
+            je n'ai pas le visage hyper symétrique, sur le spectre introverti/extraverti je suis plutôt côté introverti, en gros :<br/>
             <span class="font-weight-bold">Je ne pense pas être un bon "match" pour faire de la télé.</span></p>`,
-        mbc5: `<p>Et pourtant ! Après quelques jours / semaines, mon contact chez MBC me \\confirme que tout est bon de leur
-            côté et que je vais pouvoir tourner l'épisode dans les jours à venir !<br/> Quelle chance, je n'en reviens pas.
+        mbc5: `<p>Et pourtant ! Après quelques jours / semaines, mon contact chez MBC me confirme que tout est bon de leur
+            côté et que je vais pouvoir tourner l'épisode dans les jours à venir !<br/> Quelle chance ! Ce qui m'arrive est complètement dingue.
             Je suis bien super hyper excité, mais il faut l'avouer également un peu anxieux. Passer devant des centaines
-            de milliers (voir millions) de personne quand on a pas une confiance en soi démesurée, ce n'est pas le chose
+            de milliers (voir millions) de personnes quand on a pas une confiance en soi démesurée, ce n'est pas le chose
             la plus évidente au monde haha.</p>
           <span>Je ne vais pas écrire les détails du tournage dans ce billet, il est déjà trop long et indigeste, mais promis,
             si ça vous intéresse, je l'écrirais du mieux que je peux afin de vraiment vous faire vivre les coulisses de
@@ -138,12 +136,12 @@ const mbcMessages = {
             - Chercher les termes 'Welcome first time' ou directement '어서와 한국은 처음이지'</span>`,
         mbc5_2: `<span>si vous avez un clavier Coréen (ou le copier depuis ce billet 🤭)<br/>
             - Episode 40 de la saison 2021, date exacte : 2021-10-07<br/>
-            - La première partie est sur un Américain, je suis présenet dans l'intro et en deuxième partie d'épisode<br/>
+            - La première partie est sur un Américain, je suis présent dans l'intro et en deuxième partie d'épisode<br/>
             - Préparer vous à "cringe" sévèrement, perso je n'arrive pas à regarder l'épisode tellement je
             suis ridiculus par moment 😂 (mais bon askip' ça ne tue pas).</span>`,
         mbc6: `<p>En bonus: Quelques images de l'épisode et du tournage. Je n'avais pas vraiment le droit de prendre des photos
-            durant le shooting, mais j'ai insister pour quand même prendre quelques-unes.</p>`,
-        mbc7: `<p><i>Au moment où j'écris ce billet, j'ai tourné un autre épisode dispo le 6 Janvier 2022 sur Netflix.</i></p>
+            durant le shooting, mais j'ai insisté pour quand même prendre quelques-unes.</p>`,
+        mbc7: `<p><i>Au moment où j'écris ce billet, j'ai tourné un autre épisode dispo le 6 Janvier 2022 sur Netflix ! .</i></p>
           <p>Faite moi vos retours les plus honnête sur mon insta si vous avez vu l'épisode :
             (<a href="https://www.instagram.com/kebinou_korea/" target="_blank">@kebinou_korea</a>).
             Ca me fera énormément plaisir ❤. Bisous.</p>`,
