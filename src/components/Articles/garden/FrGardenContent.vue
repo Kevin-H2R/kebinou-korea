bbdd9a45fdb1dcc61683b1449f59265e62f6a55e
Kevin <herrbach.kevin@gmail.com>
2022-01-06 18:27:38 +0900

    Starts adding content to french version of garden article

--- src/components/Articles/garden/FrGardenContent.vue
+++ src/components/Articles/garden/FrGardenContent.vue
@@ -3,8 +3,14 @@
     <v-col cols="12" sm="10" md="8" lg="6">
       <p class="article__p"><vocab-word v-bind="vocab.today"/> je vous amène dans un endroit <b>féerique</b>.
         Et je n'exagère rien! Le Jardin du Matin Calme est un arboretum situé à quelques dizaines de kilomètre
-        de Séoul. J'y suis allé en train/bus, 2H15 depuis Hongdae.
+        de Séoul, à 가평 (Gapyeong). J'y suis allé en train/bus, 2H15 depuis Hongdae, il y a également l'ITX (train express) qui ne
+        met qu'environ 1h à y aller mais il faut réserver sa place à l'avance.</p>
+
+      <p>En résumé: 30000m² de superficie, 5000 types de plantes, 20 sections différentes, 600000 visiteurs / an.
+        Il y a de quoi faire !<br/>
       </p>
+      <p></p>
+      <p></p>
     </v-col>
   </v-row>
 </template>
